65f08a28f997356e1063a3da7307eb0a30bb05c8
Doraemon <42112059+dora-world@users.noreply.github.com>
2021-10-23 08:20:27 +0800

    Create Test.php
    例子

---
+++ class/Test.php
@@ -0,0 +1,26 @@
+<?php
+
+class Test
+{
+    private $cleanstring;
+    //run为自动执行函数，需设置public
+    public function run()
+    {
+        $this->_get_data();
+        $this->_check();
+        $this->_start();
+    }
+
+    private function _get_data(){
+        $this->cleanstring = input("name");
+    }
+    private function _check(){
+        no_empty($this->cleanstring, "name不可为空");
+    }
+    private function _start(){
+        success(array(
+            "code"=>200,
+            "message"=>"你的名字是".$this->cleanstring
+        ));
+    }
+}
