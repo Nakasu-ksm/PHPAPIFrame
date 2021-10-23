0f2dccf74bc6aaf632656351eb40d42e62891bc8
Doraemon <42112059+dora-world@users.noreply.github.com>
2021-10-23 08:11:06 +0800

    Create Config.php
    全局CONFIG

---
+++ lib/Config.php
@@ -0,0 +1,14 @@
+<?php
+define("API_VISION","1.0");
+
+/** 数据库配置 */
+define("HOST","127.0.0.1");
+define("PORT","3306");
+define("USERNAME","root");
+define("PASSWORD","password");
+define("DBNAME","root");
+define("ACCESSKEY","yourkey");
+
+/**
+ * 支持的类型
+ */
